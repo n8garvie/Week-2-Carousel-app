599342cfe928cf4f4010fdae8bd5d377282a88e7
Nathan Garvie <nathang87@gmail.com>
2015-10-30 16:49:14 -0700

    Finished sign in view with verification

--- Carousel/SignInViewController.swift
+++ Carousel/SignInViewController.swift
@@ -8,43 +8,136 @@
 
 import UIKit
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var fieldParentView: UIView!
+    @IBOutlet weak var buttonParentView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var signInButtonImage: UIImageView!
     @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var loginNavBar: UIImageView!
+    @IBOutlet weak var loginIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
         scrollView.contentSize = CGSize(width: 320, height: 600)
         
-        // Do any additional setup after loading the view.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
-   
-    @IBAction func onUsernameEdit(sender: AnyObject) {
-        scrollView.contentOffset.y = 70
+    
+    override func viewWillAppear(animated: Bool) {
+        // Set initial transform values 20% of original size
+        let transform = CGAffineTransformMakeScale(0.2, 0.2)
+        // Apply the transform properties of the views
+        loginNavBar.transform = transform
+        fieldParentView.transform = transform
+        // Set the alpha properties of the views to transparent
+        loginNavBar.alpha = 0
+        fieldParentView.alpha = 0
+    }
+    
+    // The main view appeared...
+    override func viewDidAppear(animated: Bool) {
+        //Animate the code within over 0.3 seconds...
+        UIView.animateWithDuration(0.3) { () -> Void in
+            // Return the views transform properties to their default states.
+            self.fieldParentView.transform = CGAffineTransformIdentity
+            self.loginNavBar.transform = CGAffineTransformIdentity
+            // Set the alpha properties of the views to fully opaque
+            self.fieldParentView.alpha = 1
+            self.loginNavBar.alpha = 1
+        }
+    }
+    
+    
+    func keyboardWillShow(notification: NSNotification!) {
+        // Calculate the maximum scrollview content Offset y
+        let maxContentOffsetY = scrollView.contentSize.height - scrollView.frame.size.height
+        // Scroll the scrollview up to the maximum contentOffset
+        scrollView.contentOffset.y = maxContentOffsetY
+        // Enable scrolling while keyboard is shown
+        scrollView.scrollEnabled = true
         
         UIView.animateWithDuration(0.25, animations: {
-            self.signInButton.transform = CGAffineTransformMakeTranslation( 0, -190)
-            self.signInButtonImage.transform = CGAffineTransformMakeTranslation( 0, -190)
-        })
-        
-    }
-   
-    @IBAction func onPasswordEdit(sender: AnyObject) {
-        scrollView.contentOffset.y = 70
-        
-        UIView.animateWithDuration(0.25, animations: {
-            self.signInButton.transform = CGAffineTransformMakeTranslation( 0, -190)
-            self.signInButtonImage.transform = CGAffineTransformMakeTranslation( 0, -190)
+            self.buttonParentView.transform = CGAffineTransformMakeTranslation( 0, -100)
+            
         })
     }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        // Move the buttons back down to it's original position
+        buttonParentView.transform = CGAffineTransformIdentity
+        // Return the ScrollView to it's original position
+        scrollView.contentOffset.y = 0
+        // Disable scrolling when keyboard is hidden
+        scrollView.scrollEnabled = false
+    }
+
     
     @IBAction func onTapBack(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    
+    // The scrollView is in the proccess of scrolling...
+    @IBAction func scrollViewDidScroll(scrollView: UIScrollView) {
+        // If the scrollView has been scrolled down by 50px or more...
+        if scrollView.contentOffset.y <= -50 {
+            // Hide the keyboard
+            view.endEditing(true)
+            self.loginIndicator.stopAnimating()
+        }
+        print("Scroll Happened")
+        
+    }
+    
+    // tap away from fields to dismiss keyboard
+    @IBAction func didTap(sender: AnyObject) {
+        view.endEditing(true)
+        self.loginIndicator.stopAnimating()
+    }
+    
+    @IBAction func didTapSignIn(sender: AnyObject) {
+        if (usernameField.text!.isEmpty || passwordField.text!.isEmpty) {
+            let alertController = UIAlertController(title: "That's empty boss", message: "", preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "I'm horrible", style: .Cancel) { (action) in
+                // handle cancel response here. Doing nothing will dismiss the view.
+            }
+            // add the cancel action to the alertController
+            alertController.addAction(cancelAction)
 
+            presentViewController(alertController, animated: true) {
+                // optional code for what happens after the alert controller has finished presenting
+            }
+        }
+        else if (usernameField.text == "nateg@fb.com" && passwordField.text == "password") {
+            self.loginIndicator.startAnimating()
+            delay(2, closure: { () -> () in
+                self.loginIndicator.stopAnimating()
+                self.performSegueWithIdentifier("tutorialSegue", sender: self)
+            })
+        }
+        else {
+            let alertController = UIAlertController(title: "Wrong password or username", message: "", preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "On it", style: .Cancel) { (action) in
+                // handle cancel response here. Doing nothing will dismiss the view.
+            }
+            // add the cancel action to the alertController
+            alertController.addAction(cancelAction)
+            self.loginIndicator.startAnimating()
+            delay(2, closure: { () -> () in
+                self.presentViewController(alertController, animated: true) {
+                    // optional code for what happens after the alert controller has finished presenting
+                }
+                self.loginIndicator.stopAnimating()
+            })
+        }
+        
+    }
     
     
     
